5d6de4f27e9943ff54bf102dc6c81e064c34faff
Andrei Surdu <smartik89@gmail.com>
2017-04-22 23:30:15 +0300

    Code refactoring

--- engine/Widget.class.php
+++ engine/Widget.class.php
@@ -1,7 +1,7 @@
 <?php 
 namespace SocialProfiles;
 
-class WidgetSocialProfiles extends WidgetBase{
+class Widget extends WidgetBase{
 	
 	public function settings(){
 		return array(
@@ -19,11 +19,7 @@ class WidgetSocialProfiles extends WidgetBase{
 
 		$this->addField( 'icon_size', 'select', $instance, array(
 			'label'   => __( 'Icon size', 'social-profiles' ),
-			'options' => apply_filters( 'zsp_icon_size', array(
-				''            => __( 'Default', 'social-profiles' ),
-				'large'       => __( 'Large', 'social-profiles' ),
-				'extra-large' => __( 'Extra large', 'social-profiles' ),
-			)),
+			'options' => ZSP()->config( 'icon_size' ),
 			'default' => 'large',
 			'class' => 'widefat',
 			'grid' => 4,
@@ -31,23 +27,25 @@ class WidgetSocialProfiles extends WidgetBase{
 
 		$this->addField( 'icon_shape', 'select', $instance, array(
 			'label'   => __( 'Shape', 'social-profiles' ),
-			'options' => apply_filters( 'zsp_icon_shape', array(
-				''          => __( 'Default', 'social-profiles' ),
-				'burst'     => __( 'Burst', 'social-profiles' ),
-				'burst-alt' => __( 'Burst alt', 'social-profiles' ),
-			)),
+			'options' => ZSP()->config( 'icon_shape' ),
 			'class' => 'widefat',
 			'grid' => 4
 		));
 
 		$this->addField( 'icon_radius', 'select', $instance, array(
 			'label'   => __( 'Radius', 'social-profiles' ),
-			'options' => apply_filters( 'zsp_icon_radius', array(
-				''          => __( 'Default', 'social-profiles' ),
-				'rounded'   => __( 'Rounded', 'social-profiles' ),
-				'soft'      => __( 'Soft', 'social-profiles' ),
-				'square'    => __( 'Square', 'social-profiles' ),
-			)),
+			'options' => ZSP()->config( 'icon_radius' ),
+			'default' => 'rounded',
+			'class' => 'widefat',
+			'grid' => 4
+		));
+
+		$this->addField( 'list_style', 'select', $instance, array(
+			'label'   => __( 'List style', 'social-profiles' ),
+			'options' => array(
+				'networks_list' => __( 'Networks list', 'social-profiles' ),
+				'icons_list'    => __( 'Icons list', 'social-profiles' ),
+			),
 			'default' => 'rounded',
 			'class' => 'widefat',
 			'grid' => 4
@@ -66,6 +64,7 @@ class WidgetSocialProfiles extends WidgetBase{
 		$icon_size   = !empty( $instance[ 'icon_size' ] ) ? ' '. sanitize_html_class( $instance[ 'icon_size' ] ) : '';
 		$icon_shape  = !empty( $instance[ 'icon_shape' ] ) ? ' '. sanitize_html_class( $instance[ 'icon_shape' ] ) : '';
 		$icon_radius = !empty( $instance[ 'icon_radius' ] ) ? ' '. sanitize_html_class( $instance[ 'icon_radius' ] ) : '';
+		$list_style  = !empty( $instance[ 'list_style' ] ) ? $instance[ 'list_style' ] : '';
 
 		/* Prepare icon class
 		--------------------------*/
@@ -73,14 +72,26 @@ class WidgetSocialProfiles extends WidgetBase{
 
 		/* Loop
 		------------*/
-		foreach ( ZSP()->brands as $b => $v) {
-			echo '<a href="#" class="network">
-				<span  class="sp-icon-'. $b . $class .'"><i></i></span>
-				<div class="details">
-					<div class="on">Follow us on</div>
-					<div class="title">'. ( isset( $v[1] ) ? $v[1] : ucfirst( $b ) ) .'</div>
-				</div>
-			</a>';
+		if( 'icons_list' === $list_style ){
+			foreach ( ZSP()->brands() as $b => $v) {
+				echo '<a href="#"> 
+					<span class="sp-icon-'. $b . $class .'" 
+						title="'. esc_attr( isset( $v[1] ) ? $v[1] : ucfirst( $b ) ) .'">
+						<i></i>
+					</span>
+				</a>';
+			}
+		}
+		else{
+			foreach ( ZSP()->brands() as $b => $v) {
+				echo '<a href="#" class="network">
+					<span  class="sp-icon-'. $b . $class .'"><i></i></span>
+					<div class="details">
+						<div class="on">Follow us on</div>
+						<div class="title">'. ( isset( $v[1] ) ? $v[1] : ucfirst( $b ) ) .'</div>
+					</div>
+				</a>';
+			}
 		}
 
 		echo $this->closeWidget( $args );
